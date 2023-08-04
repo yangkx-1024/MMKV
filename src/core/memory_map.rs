69360c73607f123990bc0051f0610ead7cfe0694
kexuan.yang <kexuan.yang@gmail.com>
2023-08-04 16:34:14 +0800

    Refactor buffer, kv_store and memory_map
    Change-Id: Ide475558e5f30c6ac18b0c8001bbf57a20de6c3a
    Signed-off-by: kexuan.yang <kexuan.yang@gmail.com>

--- src/core/memory_map.rs
+++ src/core/memory_map.rs
@@ -1,57 +1,80 @@
 use std::fs::File;
+use std::ops::Range;
 
 use memmap2::MmapMut;
-
-use crate::core::kv_store::ContentContainer;
+use crate::core::buffer::Buffer;
 
 const _LEN_OFFSET: usize = 8;
 
 #[derive(Debug)]
-pub struct MemoryMap {
-    inner_map: MmapMut,
-}
+pub struct MemoryMap(MmapMut);
 
 impl MemoryMap {
     pub fn new(file: &File) -> Self {
-        MemoryMap {
-            inner_map: unsafe { MmapMut::map_mut(file) }.unwrap(),
+        MemoryMap(unsafe { MmapMut::map_mut(file) }.unwrap())
+    }
+
+    pub fn append(&mut self, value: Vec<u8>) -> std::io::Result<()> {
+        let data_len = value.len();
+        let start = self.len();
+        let content_len = start - _LEN_OFFSET;
+        let end = data_len + start;
+        let new_content_len = data_len + content_len;
+        self.0[0.._LEN_OFFSET].copy_from_slice(new_content_len.to_be_bytes().as_slice());
+        self.0[start..end].copy_from_slice(value.as_slice());
+        self.0.flush()
+    }
+
+    pub fn write_all(&mut self, value: Vec<u8>) -> std::io::Result<()> {
+        let data_len = value.len();
+        let start = _LEN_OFFSET;
+        let end = start + data_len;
+        self.0[0.._LEN_OFFSET].copy_from_slice(data_len.to_be_bytes().as_slice());
+        self.0[start..end].copy_from_slice(value.as_slice());
+        self.0.flush()
+    }
+
+    pub fn len(&self) -> usize {
+        usize::from_be_bytes(
+            self.0[0.._LEN_OFFSET].try_into().unwrap()
+        ) + _LEN_OFFSET
+    }
+
+    fn read(&self, range: Range<usize>) -> &[u8] {
+        self.0[range].as_ref()
+    }
+}
+
+pub struct Iter<'a> {
+    mm: &'a MemoryMap,
+    start: usize,
+    end: usize,
+}
+
+impl MemoryMap {
+    pub fn iter(&self) -> Iter {
+        let start = _LEN_OFFSET;
+        let end = self.len();
+        Iter {
+            mm: self,
+            start,
+            end
         }
     }
 }
 
-impl ContentContainer for MemoryMap {
-    fn max_len(&self) -> usize {
-        self.inner_map.len() - _LEN_OFFSET
-    }
+impl <'a> Iterator for Iter<'a> {
+    type Item = Buffer;
 
-    fn content_len(&self) -> usize {
-        usize::from_be_bytes(
-            self.inner_map[0.._LEN_OFFSET].try_into().unwrap()
-        )
-    }
-
-    fn append(&mut self, value: Vec<u8>) -> std::io::Result<()> {
-        let data_len = value.len();
-        let content_len = self.content_len();
-        let start = content_len + _LEN_OFFSET;
-        let end = data_len + start;
-        let new_content_len = data_len + content_len;
-        self.inner_map[0.._LEN_OFFSET].copy_from_slice(new_content_len.to_be_bytes().as_slice());
-        self.inner_map[start..end].copy_from_slice(value.as_slice());
-        self.inner_map.flush()
-    }
-
-    fn write_all(&mut self, value: Vec<u8>) -> std::io::Result<()> {
-        let data_len = value.len();
-        let start = _LEN_OFFSET;
-        let end = start + data_len;
-        self.inner_map[0.._LEN_OFFSET].copy_from_slice(data_len.to_be_bytes().as_slice());
-        self.inner_map[start..end].copy_from_slice(value.as_slice());
-        self.inner_map.flush()
-    }
-
-    fn read(&self, offset: usize) -> &[u8] {
-        self.inner_map[(offset + _LEN_OFFSET)..].as_ref()
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start >= self.end {
+            return None
+        }
+        let (buffer, len) = Buffer::from_encoded_bytes(
+            self.mm.read(self.start..self.end).as_ref()
+        );
+        self.start += len as usize;
+        Some(buffer)
     }
 }
 
@@ -59,37 +82,56 @@ impl ContentContainer for MemoryMap {
 mod tests {
     use std::fs;
     use std::fs::OpenOptions;
-
-    use crate::core::kv_store::ContentContainer;
+    use crate::core::buffer::Buffer;
 
     use super::MemoryMap;
 
     #[test]
     fn test_mmap() {
-        let _ = fs::remove_file("memory_map_test.txt");
-        let file = OpenOptions::new().create(true).write(true).read(true).open("memory_map_test.txt").unwrap();
-        file.set_len(1024).unwrap();
-        let mut mm = MemoryMap::new(&file);
-        assert_eq!(mm.max_len(), 1016);
-        assert_eq!(mm.content_len(), 0);
-        mm.append(vec![1u8, 2u8, 3u8]).unwrap();
-        assert_eq!(mm.content_len(), 3);
-        mm.append(vec![4u8]).unwrap();
-        assert_eq!(mm.content_len(), 4);
+        let mut mm = new_mm("test_mmap");
+        assert_eq!(mm.len(), 8);
+        mm.append(vec![1, 2, 3]).unwrap();
+        mm.append(vec![4]).unwrap();
+        assert_eq!(mm.len(), 12);
 
-        let read = mm.read(0);
-        assert_eq!(read[0], 1u8);
-        assert_eq!(read[1], 2u8);
-        let read = mm.read(mm.content_len() - 1);
-        assert_eq!(read[0], 4u8);
+        let read = mm.read(8..10);
+        assert_eq!(read.len(), 2);
+        assert_eq!(read[0], 1);
+        assert_eq!(read[1], 2);
+        let read = mm.read(mm.len() - 1..mm.len());
+        assert_eq!(read[0], 4);
 
         mm.write_all(vec![5, 4, 3, 2, 1]).unwrap();
-        assert_eq!(mm.content_len(), 5);
-        let read = mm.read(0);
+        assert_eq!(mm.len(), 13);
+        let read = mm.read(8..9);
         assert_eq!(read[0], 5);
 
-        let read = mm.read(1);
+        let read = mm.read(9..10);
         assert_eq!(read[0], 4);
-        let _ = fs::remove_file("memory_map_test.txt");
+        let _ = fs::remove_file("test_mmap");
+    }
+
+    #[test]
+    fn test_mmap_iterator() {
+        let mut mm = new_mm("test_mmap_iterator");
+        let mut buffers: Vec<Buffer> = vec![];
+        for i in 0..10 {
+            let buffer = Buffer::from_i32(&i.to_string(), i);
+            mm.append(buffer.to_bytes()).unwrap();
+            buffers.push(buffer);
+        }
+        let mut index = 0;
+        for i in mm.iter() {
+            assert_eq!(buffers[index], i);
+            index += 1;
+        }
+        let _ = fs::remove_file("test_mmap_iterator");
+    }
+
+    fn new_mm(file_name: &str) -> MemoryMap {
+        let _ = fs::remove_file(file_name);
+        let file = OpenOptions::new().create(true).write(true).read(true).open(file_name).unwrap();
+        file.set_len(1024).unwrap();
+        MemoryMap::new(&file)
     }
 }
