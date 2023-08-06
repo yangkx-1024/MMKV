92a8bd82dcd6141b925130a42770b9b367d1b210
kexuan.yang <kexuan.yang@gmail.com>
2023-08-06 17:03:58 +0800

    Add crypt to buffer
    Change-Id: I81d243ede4111bfc72c27b4ab062780261b11ba0
    Signed-off-by: kexuan.yang <kexuan.yang@gmail.com>

--- src/mmkv.rs
+++ src/mmkv.rs
@@ -1,7 +1,7 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::sync::OnceLock;
 
-use crate::core::buffer::{Buffer, Decoder};
+use crate::core::buffer::Buffer;
 use crate::core::kv_store::KvStore;
 
 const _DEFAULT_FILE_NAME: &str = "mini_mmkv";
@@ -13,10 +13,21 @@ static mut _STORE: OnceLock<KvStore> = OnceLock::new();
 Rust version of MMKV.
 
 Examples:
+
+Using directly:
 ```
 use mmkv::MMKV;
 
 MMKV::initialize(".");
+MMKV::put_i32("key1", 1);
+assert_eq!(MMKV::get_i32("key1"), Some(1));
+```
+
+Using with encryption:
+```
+use mmkv::MMKV;
+
+MMKV::initialize_with_encrypt_key(".", "88C51C536176AD8A8EE4A06F62EE897E");
 MMKV::put_i32("key1", 1);
 assert_eq!(MMKV::get_i32("key1"), Some(1));
 ```
@@ -34,6 +45,30 @@ impl MMKV {
     calling initialize multiple times will also cause panic.
      */
     pub fn initialize(dir: &str) {
+        let file_path = MMKV::resolve_file_path(dir);
+        unsafe {
+            _STORE.set(
+                KvStore::new(file_path.as_path(), _PAGE_SIZE)
+            ).expect("initialize more than one time");
+        }
+    }
+
+    /**
+    See [initialize](MMKV::initialize)
+
+    Initialize the MMKV instance with a writeable directory,
+    and a credential for encryption(enable content encryption).
+     */
+    pub fn initialize_with_encrypt_key(dir: &str, key: &str) {
+        let file_path = MMKV::resolve_file_path(dir);
+        unsafe {
+            _STORE.set(
+                KvStore::new_with_encrypt_key(file_path.as_path(), _PAGE_SIZE, key)
+            ).expect("initialize more than one time");
+        }
+    }
+
+    fn resolve_file_path(dir: &str) -> PathBuf {
         let path = Path::new(dir);
         if !path.is_dir() {
             panic!("path {}, is not dir", dir);
@@ -42,40 +77,35 @@ impl MMKV {
         if metadata.permissions().readonly() {
             panic!("path {}, is readonly", dir);
         }
-        let file_path = path.join(_DEFAULT_FILE_NAME);
-        unsafe {
-            _STORE.set(
-                KvStore::new(file_path.as_path(), _PAGE_SIZE)
-            ).expect("initialize more than one time");
-        }
+        path.join(_DEFAULT_FILE_NAME)
     }
 
     pub fn put_str(key: &str, value: &str) {
-        _ensure_mut_store().write(key, Buffer::from_str(key, value));
+        crate::mut_kv_store!().write(key, Buffer::from_str(key, value));
     }
 
     pub fn get_str(key: &str) -> Option<&str> {
-        _ensure_store().get(key).map(|buffer| {
+        crate::kv_store!().get(key).map(|buffer| {
             buffer.decode_str()
         }).flatten()
     }
 
     pub fn put_i32(key: &str, value: i32) {
-        _ensure_mut_store().write(key, Buffer::from_i32(key, value));
+        crate::mut_kv_store!().write(key, Buffer::from_i32(key, value));
     }
 
     pub fn get_i32(key: &str) -> Option<i32> {
-        _ensure_store().get(key).map(|buffer| {
+        crate::kv_store!().get(key).map(|buffer| {
             buffer.decode_i32()
         }).flatten()
     }
 
     pub fn put_bool(key: &str, value: bool) {
-        _ensure_mut_store().write(key, Buffer::from_bool(key, value));
+        crate::mut_kv_store!().write(key, Buffer::from_bool(key, value));
     }
 
     pub fn get_bool(key: &str) -> Option<bool> {
-        _ensure_store().get(key).map(|buffer| {
+        crate::kv_store!().get(key).map(|buffer| {
             buffer.decode_bool()
         }).flatten()
     }
@@ -86,19 +116,29 @@ impl MMKV {
     `KvStore { file_size: 1024, key_count: 4, content_len: 107 }`
      */
     pub fn dump() -> String {
-        _ensure_store().to_string()
+        crate::kv_store!().to_string()
     }
 }
 
-fn _ensure_store() -> &'static KvStore {
-    unsafe {
-        _STORE.get().expect("not initialize")
+#[macro_export]
+macro_rules! kv_store {
+    () => {
+        {
+            unsafe {
+                _STORE.get().expect("not initialize")
+            }
+        }
     }
 }
 
-fn _ensure_mut_store() -> &'static mut KvStore {
-    unsafe {
-        _STORE.get_mut().expect("not initialize")
+#[macro_export]
+macro_rules! mut_kv_store {
+    () => {
+        {
+            unsafe {
+                _STORE.get_mut().expect("not initialize")
+            }
+        }
     }
 }
 
@@ -109,8 +149,10 @@ mod tests {
     use super::MMKV;
 
     #[test]
-    fn test_put_and_get() {
-        MMKV::initialize(".");
+    fn test_put_and_get_with_encryption() {
+        let _ = fs::remove_file("mini_mmkv");
+        let _ = fs::remove_file("mmkv_meta");
+        MMKV::initialize_with_encrypt_key(".", "88C51C536176AD8A8EE4A06F62EE897E");
         MMKV::put_i32("first", 1);
         MMKV::put_i32("second", 2);
         assert_eq!(MMKV::get_i32("first"), Some(1));
@@ -130,5 +172,6 @@ mod tests {
         assert_eq!(MMKV::get_bool("second"), Some(false));
         println!("{}", MMKV::dump());
         let _ = fs::remove_file("mini_mmkv");
+        let _ = fs::remove_file("mmkv_meta");
     }
 }
