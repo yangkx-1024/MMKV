a5960bfa62d4fd43e26313eaacb7f40ac7645073
kexuan.yang <kexuan.yang@gmail.com>
2023-08-06 17:03:58 +0800

    Add crypt to buffer
    Change-Id: I81d243ede4111bfc72c27b4ab062780261b11ba0
    Signed-off-by: kexuan.yang <kexuan.yang@gmail.com>

--- src/core/crc.rs
+++ src/core/crc.rs
@@ -1,15 +1,71 @@
 use crc::{Crc, CRC_8_AUTOSAR};
+use crate::core::buffer::{Buffer, Decoder, Encoder, Take};
 
-pub const CRC8: Crc<u8> = Crc::<u8>::new(&CRC_8_AUTOSAR);
+const CRC8: Crc<u8> = Crc::<u8>::new(&CRC_8_AUTOSAR);
+
+#[derive(Debug)]
+pub struct CrcBuffer(Option<Buffer>);
+
+impl CrcBuffer {
+    pub fn new() -> Self {
+        CrcBuffer(None)
+    }
+
+    pub fn new_with_buffer(buffer: Buffer) -> Self {
+        CrcBuffer(Some(buffer))
+    }
+}
+
+impl Encoder for CrcBuffer {
+    fn encode_to_bytes(&self) -> Vec<u8> {
+        let bytes_to_write = self.0.as_ref().unwrap().to_bytes();
+        let sum = CRC8.checksum(bytes_to_write.as_slice());
+        let len = bytes_to_write.len() as u32 + 1;
+        let mut data = len.to_be_bytes().to_vec();
+        data.extend_from_slice(bytes_to_write.as_slice());
+        data.push(sum);
+        data
+    }
+}
+
+impl Decoder for CrcBuffer {
+    fn decode_bytes(&mut self, data: &[u8]) -> u32 {
+        let item_len = u32::from_be_bytes(
+            data[0..4].try_into().unwrap()
+        );
+        let bytes_to_decode = &data[4..(3 + item_len as usize)];
+        let sum = data[3 + item_len as usize];
+        if CRC8.checksum(bytes_to_decode) == sum {
+            self.0 = Some(Buffer::from_encoded_bytes(bytes_to_decode))
+        }
+        4 + item_len
+    }
+}
+
+impl Take for CrcBuffer {
+    fn take(self) -> Option<Buffer> {
+        self.0
+    }
+}
+
+impl PartialEq for CrcBuffer {
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::core::crc::CRC8;
+    use crate::core::buffer::{Buffer, Decoder, Encoder};
+    use crate::core::crc::CrcBuffer;
 
     #[test]
-    fn test_crc32() {
-        let mut digest = CRC8.digest();
-        digest.update(b"123456789");
-        assert_eq!(digest.finalize(), 0xdf);
+    fn test_crc_buffer() {
+        let buffer = Buffer::from_i32("key", 1);
+        let buffer = CrcBuffer::new_with_buffer(buffer);
+        let bytes = buffer.encode_to_bytes();
+        let mut copy = CrcBuffer::new();
+        copy.decode_bytes(bytes.as_slice());
+        assert_eq!(copy, buffer)
     }
 }
--- src/core/kv_store.rs
+++ src/core/kv_store.rs
@@ -1,36 +1,63 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
+use std::fs;
 use std::fs::{File, OpenOptions};
-use std::path::Path;
+use std::io::{Read, Write};
+use std::path::{Path, PathBuf};
+use std::rc::Rc;
 use std::sync::RwLock;
-
-use crate::core::buffer::{Buffer, BufferResult};
+use crate::core::buffer::{Buffer, Encoder, Take};
+use crate::core::crc::CrcBuffer;
+use crate::core::crypt::{Crypt, CryptBuffer};
 use crate::core::memory_map::MemoryMap;
+
+const _META_FILE_NAME: &str = "mmkv_meta";
 
 #[derive(Debug)]
 pub struct KvStore {
     _kv_map: HashMap<String, Buffer>,
     _file: File,
+    _meta_file_path: PathBuf,
     _mm: RwLock<MemoryMap>,
     _file_size: u64,
     _page_size: u64,
+    _crypt: Option<Rc<RefCell<Crypt>>>,
 }
 
 impl KvStore {
     pub fn new(path: &Path, page_size: u64) -> Self {
+        KvStore::new_kv_store(path, page_size, None)
+    }
+
+    pub fn new_with_encrypt_key(path: &Path, page_size: u64, key: &str) -> Self {
+        KvStore::new_kv_store(path, page_size, Some(key))
+    }
+
+    fn new_kv_store(path: &Path, page_size: u64, key: Option<&str>) -> Self {
         let file = OpenOptions::new().read(true).write(true).create(true).open(path).unwrap();
         let mut file_len = file.metadata().unwrap().len();
         if file_len == 0 {
             file_len += page_size;
             file.set_len(file_len).unwrap();
         }
+        let meta_file_path = path.parent().expect("unable to access dir").join(_META_FILE_NAME);
+        let crypt = key.map(|raw_key| {
+            let key = hex::decode(raw_key).unwrap();
+            Rc::new(RefCell::new(
+                KvStore::init_crypt(&meta_file_path, key.as_slice())
+            ))
+        });
+
         let mm = MemoryMap::new(&file);
         let mut store = KvStore {
             _kv_map: HashMap::new(),
             _file: file,
+            _meta_file_path: meta_file_path,
             _mm: RwLock::new(mm),
             _file_size: file_len,
             _page_size: page_size,
+            _crypt: crypt,
         };
         store.init();
         store
@@ -39,25 +66,72 @@ impl KvStore {
     fn init(&mut self) {
         // acquire write lock
         let mm = self._mm.read().unwrap();
-        for buffer in mm.iter() {
-            if let BufferResult::Ok(data) = buffer {
+        let decode = |buffer: Option<Buffer>| {
+            if let Some(data) = buffer {
                 self._kv_map.insert(data.key().to_string(), data);
             }
+        };
+        match self._crypt.as_ref() {
+            Some(crypt) => {
+                mm.iter(|| CryptBuffer::new(crypt.clone())).for_each(decode)
+            }
+            None => {
+                mm.iter(|| CrcBuffer::new()).for_each(decode)
+            }
+        };
+    }
+
+    fn init_crypt(meta_path: &PathBuf, key: &[u8]) -> Crypt {
+        if meta_path.exists() {
+            let mut nonce_file = OpenOptions::new().read(true).open(meta_path).unwrap();
+            let mut nonce = Vec::<u8>::new();
+            nonce_file.read_to_end(&mut nonce).expect("failed to read nonce file");
+            Crypt::new_with_nonce(key.try_into().unwrap(), nonce.as_slice())
+        } else {
+            let crypt = Crypt::new(key.try_into().unwrap());
+            let mut nonce_file = OpenOptions::new().create(true).write(true)
+                .open(meta_path)
+                .unwrap();
+            nonce_file.write_all(crypt.nonce().as_slice()).expect("failed to write nonce file");
+            nonce_file.sync_all().unwrap();
+            crypt
         }
     }
 
     pub fn write(&mut self, key: &str, buffer: Buffer) {
+        if let Some(crypt) = self._crypt.clone() {
+            self.write_internal(key, buffer, |buffer: Buffer| CryptBuffer::new_with_buffer(buffer, crypt.clone()))
+        } else {
+            self.write_internal(key, buffer, |buffer: Buffer| CrcBuffer::new_with_buffer(buffer))
+        };
+    }
+
+    fn write_internal<T, F>(&mut self, key: &str, raw_buffer: Buffer, transform: F) where T: Encoder + Take, F: Fn(Buffer) -> T {
         // acquire write lock
         let mut mm = self._mm.write().unwrap();
-        let data = buffer.to_bytes();
+        let buffer = transform(raw_buffer);
+        let mut data = buffer.encode_to_bytes();
         let mut target_end = data.len() + mm.len();
         if target_end as u64 > self._file_size {
             // trim the file, drop the duplicate items
+            let mut crypt_changed = false;
+            if let Some(crypt) = self._crypt.clone() {
+                let _ = fs::remove_file(self._meta_file_path.to_path_buf());
+                let key = crypt.borrow().key();
+                *crypt.borrow_mut() = KvStore::init_crypt(
+                    &self._meta_file_path, key.as_slice(),
+                );
+                crypt_changed = true;
+            }
             let mut vec: Vec<u8> = vec!();
-            for (_, buffer) in self._kv_map.iter() {
-                vec.extend_from_slice(buffer.to_bytes().as_slice());
+            for buffer in self._kv_map.values() {
+                let buffer = transform(buffer.clone());
+                vec.extend_from_slice(buffer.encode_to_bytes().as_slice());
             }
             mm.write_all(vec).unwrap();
+            if crypt_changed {
+                data = buffer.encode_to_bytes();
+            }
             target_end = data.len() + mm.len()
         }
         while target_end as u64 > self._file_size {
@@ -68,7 +142,7 @@ impl KvStore {
             *mm = MemoryMap::new(&self._file);
         }
         mm.append(data).unwrap();
-        self._kv_map.insert(key.to_string(), buffer);
+        self._kv_map.insert(key.to_string(), buffer.take().unwrap());
     }
 
     pub fn get(&self, key: &str) -> Option<&Buffer> {
@@ -103,46 +177,50 @@ mod tests {
     use std::sync::OnceLock;
     use std::thread::spawn;
 
-    use crate::core::buffer::{Buffer, Decoder};
-    use crate::core::kv_store::KvStore;
+    use crate::core::buffer::Buffer;
+    use crate::core::crc::CrcBuffer;
+    use crate::core::kv_store::{_META_FILE_NAME, KvStore};
 
     #[test]
-    fn test_kv_store() {
+    fn test_normal_kv_store() {
         let _ = fs::remove_file("test_kv_store");
-        let mut store = KvStore::new(Path::new("test_kv_store"), 100);
+        test_kv_store(|| KvStore::new(Path::new("test_kv_store"), 100));
+        let _ = fs::remove_file("test_kv_store");
+    }
+
+    #[test]
+    fn test_crypt_kv_store() {
+        let _ = fs::remove_file("test_crypt_kv_store");
+        let _ = fs::remove_file(_META_FILE_NAME);
+        test_kv_store(|| KvStore::new_with_encrypt_key(
+            Path::new("test_crypt_kv_store"),
+            100,
+            "88C51C536176AD8A8EE4A06F62EE897E",
+        ));
+        let _ = fs::remove_file("test_crypt_kv_store");
+        let _ = fs::remove_file(_META_FILE_NAME);
+    }
+
+    fn test_kv_store<F>(f: F) where F: Fn() -> KvStore {
+        let mut store = f();
         store.write("key1", Buffer::from_i32("key1", 1));
-        assert_eq!(store._mm.read().unwrap().len(), 25);
         assert_eq!(store.get("key1").unwrap().decode_i32().unwrap(), 1);
         assert_eq!(store.get("key2").is_none(), true);
         store.write("key2", Buffer::from_i32("key2", 2));
         assert_eq!(store.get("key2").unwrap().decode_i32().unwrap(), 2);
         store.write("key3", Buffer::from_i32("key3", 3));
         assert_eq!(store.get("key3").unwrap().decode_i32().unwrap(), 3);
-        assert_eq!(store._mm.read().unwrap().len(), 59);
 
-        store.write("key1", Buffer::from_i32("key1", 4));
-        store.write("key2", Buffer::from_i32("key2", 5));
-        store.write("key3", Buffer::from_i32("key3", 6));
-        assert_eq!(store.get("key1").unwrap().decode_i32().unwrap(), 4);
-        assert_eq!(store.get("key2").unwrap().decode_i32().unwrap(), 5);
-        assert_eq!(store.get("key3").unwrap().decode_i32().unwrap(), 6);
-        assert_eq!(store._mm.read().unwrap().len(), 76);
-        store.write("key1", Buffer::from_i32("key1", 1));
-        assert_eq!(store._mm.read().unwrap().len(), 93);
-        assert_eq!(store.get("key1").unwrap().decode_i32().unwrap(), 1);
-        store.write("key2", Buffer::from_i32("key2", 2));
-        assert_eq!(store._mm.read().unwrap().len(), 76);
-        assert_eq!(store.get("key2").unwrap().decode_i32().unwrap(), 2);
-        assert_eq!(store.get("key3").unwrap().decode_i32().unwrap(), 6);
-        assert_eq!(store._file_size, 100);
+        store.write("key1", Buffer::from_str("key1", "4"));
+        assert_eq!(store.get("key1").unwrap().decode_i32(), None);
+        assert_eq!(store.get("key1").unwrap().decode_str().unwrap(), "4");
 
         drop(store);
 
-        let mut store = KvStore::new(Path::new("test_kv_store"), 100);
-        assert_eq!(store.get("key1").unwrap().decode_i32().unwrap(), 1);
+        let mut store = f();
+        assert_eq!(store.get("key1").unwrap().decode_str().unwrap(), "4");
         assert_eq!(store.get("key2").unwrap().decode_i32().unwrap(), 2);
-        assert_eq!(store.get("key3").unwrap().decode_i32().unwrap(), 6);
-        assert_eq!(store._mm.read().unwrap().len(), 76);
+        assert_eq!(store.get("key3").unwrap().decode_i32().unwrap(), 3);
 
         store.write("key4", Buffer::from_i32("key4", 4));
         store.write("key5", Buffer::from_i32("key5", 5));
@@ -151,8 +229,6 @@ mod tests {
         store.write("key8", Buffer::from_i32("key8", 8));
         store.write("key9", Buffer::from_i32("key9", 9));
         assert_eq!(store.get("key9").unwrap().decode_i32().unwrap(), 9);
-        assert_eq!(store._file_size, 200);
-        let _ = fs::remove_file("test_kv_store");
     }
 
     #[test]
@@ -175,7 +251,7 @@ mod tests {
             handle.join().unwrap();
             println!("value: {}", STORE.get().unwrap().get(key).unwrap().decode_i32().unwrap());
             println!("{}", STORE.get().unwrap());
-            let count = STORE.get().unwrap()._mm.read().unwrap().iter().count();
+            let count = STORE.get().unwrap()._mm.read().unwrap().iter(|| CrcBuffer::new()).count();
             assert_eq!(count, 20000)
         }
         let _ = fs::remove_file("test_kv_store_multi_thread");
