8873bcc866905e2e6e7200a75647a9b9ebf25dbe
kexuan.yang <kexuan.yang@gmail.com>
2023-08-09 00:28:29 +0800

    Config maven uploading
    Change-Id: I63dfd59e248ce84b800df5cfe7734fee47fd7dc6
    Signed-off-by: kexuan.yang <kexuan.yang@gmail.com>

--- android/library-encrypt/build.gradle.kts
+++ android/library-encrypt/build.gradle.kts
@@ -1,6 +1,19 @@
+import java.io.FileInputStream
+import java.net.URI
+import java.util.Properties
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+    id("signing")
+}
+
+val prop = Properties().apply {
+    load(FileInputStream(File(rootProject.rootDir, "local.properties")))
+}
+prop.forEach {
+    ext[it.key as String] = it.value as String
 }
 
 android {
@@ -29,8 +42,74 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
+}
+
+publishing {
+    val artifactId = "mmkv-encrypt"
+    val version = "0.1.0"
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "net.yangkx"
+            this.artifactId = artifactId
+            this.version = version
+
+            afterEvaluate {
+                from(components["release"])
+            }
+            pom {
+                name.set(artifactId)
+                description.set("Library uses file-based mmap to store key-values")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Kexuan Yang")
+                        email.set("kexuan.yang@gmail.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/yangkx1024/MMKV")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/content/repositories/releases"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+
+            url = URI(
+                if (version.endsWith("-SNAPSHOT")) {
+                    snapshotsRepoUrl
+                } else {
+                    releasesRepoUrl
+                }
+            )
+            credentials {
+                username = prop["sonatypeUsername"] as String
+                password = prop["sonatypePassword"] as String
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications)
 }
