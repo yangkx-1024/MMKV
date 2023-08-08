0dc1a7a4a888e371d2ac23aaf068eab32d9339c7
kexuan.yang <kexuan.yang@gmail.com>
2023-08-09 00:44:37 +0800

    update README.md
    Change-Id: Ia6e6c8324a505c56f945069b4d32781150be55c7

--- android/app/src/default/java/net/yangkx/mmkv/MMKVInitializer.kt
+++ android/app/src/default/java/net/yangkx/mmkv/MMKVInitializer.kt
@@ -5,7 +5,6 @@ import android.content.Context
 object MMKVInitializer {
     fun init(context: Context) {
         val dir = context.getDir("mmkv", Context.MODE_PRIVATE)
-        if (dir.exists()) dir.delete()
         MMKV.initialize(dir.absolutePath)
     }
 }
