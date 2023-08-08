582a0e7932f82486298f71952b904ac0f3f1ccfb
kexuan.yang <kexuan.yang@gmail.com>
2023-08-09 00:44:37 +0800

    update README.md
    Change-Id: Ia6e6c8324a505c56f945069b4d32781150be55c7

--- android/app/src/encryption/java/net/yangkx/mmkv/MMKVInitializer.kt
+++ android/app/src/encryption/java/net/yangkx/mmkv/MMKVInitializer.kt
@@ -5,7 +5,6 @@ import android.content.Context
 object MMKVInitializer {
     fun init(context: Context) {
         val dir = context.getDir("mmkv", Context.MODE_PRIVATE)
-        if (dir.exists()) dir.delete()
         MMKV.initialize(dir.absolutePath, "88C51C536176AD8A8EE4A06F62EE897E")
     }
 }
