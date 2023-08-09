eac7f7b8aca75c7509251e543834fcadb3c369b0
kexuan.yang <kexuan.yang@gmail.com>
2023-08-10 05:04:40 +0800

    Allow init multi times & add API clear_data, close
    Change-Id: Ie5eb8ef6f40ceb312a3b3de8e79b5acadac2507f
    Signed-off-by: kexuan.yang <kexuan.yang@gmail.com>

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,16 +1,7 @@
-use std::fs;
-
 use mmkv::MMKV;
+use std::thread::{spawn, JoinHandle};
 
-#[test]
-fn integration() {
-    let _ = fs::remove_file("mini_mmkv");
-    let _ = fs::remove_file("mini_mmkv.meta");
-    MMKV::initialize(
-        ".",
-        #[cfg(feature = "encryption")]
-        "88C51C536176AD8A8EE4A06F62EE897E",
-    );
+fn test_api() {
     MMKV::put_i32("first", 1);
     MMKV::put_i32("second", 2);
     assert_eq!(MMKV::get_i32("first"), Some(1));
@@ -28,7 +19,35 @@ fn integration() {
     MMKV::put_bool("second", false);
     assert_eq!(MMKV::get_str("second"), None);
     assert_eq!(MMKV::get_bool("second"), Some(false));
-    println!("{}", MMKV::dump());
-    let _ = fs::remove_file("mini_mmkv");
-    let _ = fs::remove_file("mini_mmkv.meta");
+}
+
+#[test]
+fn test_multi_thread() {
+    MMKV::initialize(
+        ".",
+        #[cfg(feature = "encryption")]
+        "88C51C536176AD8A8EE4A06F62EE897E",
+    );
+    let handle = spawn(|| {
+        test_api();
+    });
+    let mut handles = Vec::<JoinHandle<()>>::new();
+
+    for i in 0..4 {
+        handles.push(spawn(move || {
+            for j in (i * 1000)..(i + 1) * 1000 {
+                let key = format!("key_{j}");
+                MMKV::put_str(&key, &key);
+            }
+        }));
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    handle.join().unwrap();
+    for i in 0..4000 {
+        let key = format!("key_{i}");
+        assert_eq!(MMKV::get_str(&key).unwrap(), &key)
+    }
+    MMKV::clear_data();
 }
