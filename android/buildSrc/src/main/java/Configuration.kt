5d2e16ff6bcf04a6207c476f6ae792c92625f725
kexuan.yang <kexuan.yang@gmail.com>
2023-08-16 12:46:55 +0800

    Add ProGuard rules for Android project
    Change-Id: I6a85d4da0a56fe3b5ac05e44134bba23718877fe
    Signed-off-by: kexuan.yang <kexuan.yang@gmail.com>

---
+++ android/buildSrc/src/main/java/Configuration.kt
@@ -0,0 +1,23 @@
+object Configuration {
+//    const val libVersion = "0.2.0.2-SNAPSHOT"
+    const val libVersion = "0.2.0.1"
+    const val groupId = "net.yangkx"
+    const val description = "Library uses file-based mmap to store key-values"
+    const val releasesRepoUrl = "https://s01.oss.sonatype.org/content/repositories/releases/"
+    const val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+    val publishUrl = if (libVersion.endsWith("-SNAPSHOT")) {
+        snapshotsRepoUrl
+    } else {
+        releasesRepoUrl
+    }
+    val licenceApache = "The Apache License, Version 2.0" to "http://www.apache.org/licenses/LICENSE-2.0.txt"
+    val licenceMit = "The MIT License" to "https://opensource.org/licenses/MIT"
+    val developer = "Kexuan Yang" to "kexuan.yang@gmail.com"
+    const val scmUrl = "https://github.com/yangkx1024/MMKV"
+}
+
+object Deps {
+    val kotlin = "androidx.core:core-ktx:1.10.1"
+    val mmkv = "net.yangkx:mmkv:${Configuration.libVersion}"
+    val mmkv_encrypt = "net.yangkx:mmkv-encrypt:${Configuration.libVersion}"
+}
