92f81df54468ec1a9635b911ede629be32ffddb7
kexuan.yang <kexuan.yang@gmail.com>
2023-11-08 22:25:55 +0800

    Add iOS support
    Change-Id: I8ffc2b83244fc0f506bf4eb8e61b32fd547f8137

---
+++ ios/MMKV/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MMKV",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MMKV",
+            targets: ["MMKV"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "MMKV", path: "./Sources/MMKV.xcframework"),
+        .testTarget(
+            name: "MMKVTests",
+            dependencies: ["MMKV"]),
+    ]
+)
