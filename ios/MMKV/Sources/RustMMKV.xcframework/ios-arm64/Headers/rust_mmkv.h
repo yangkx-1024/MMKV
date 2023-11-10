053af7042b4e569ef72c4ff820459ea072acc808
kexuan.yang <kexuan.yang@gmail.com>
2023-11-10 23:51:47 +0800

    fix memory issue off c ffi
    Change-Id: I4185b05a1280bac69f92f7cf9ab85e6b803c3388

--- ios/MMKV/Sources/RustMMKV.xcframework/ios-arm64/Headers/rust_mmkv.h
+++ ios/MMKV/Sources/RustMMKV.xcframework/ios-arm64/Headers/rust_mmkv.h
@@ -3,49 +3,65 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-typedef struct ByteSlice {
+enum Types {
+  I32,
+  STR,
+  BYTE,
+  I64,
+  F32,
+  F64,
+  ByteArray,
+  I32Array,
+  I64Array,
+  F32Array,
+  F64Array,
+};
+
+struct RawTypedArray {
+  const void *array;
+  enum Types type_token;
+  uintptr_t len;
+};
+
+struct ByteSlice {
   const uint8_t *bytes;
   uintptr_t len;
-} ByteSlice;
+};
 
-typedef struct NativeLogger {
+struct NativeLogger {
   void *obj;
-  void (*callback)(void *obj, int32_t level, struct ByteSlice content);
-} NativeLogger;
+  void (*callback)(void *obj, int32_t level, const struct ByteSlice *content);
+};
 
-typedef struct InternalError {
+struct InternalError {
   int32_t code;
   const struct ByteSlice *reason;
-} InternalError;
+};
 
-typedef struct VoidResult {
+struct RawBuffer {
+  const void *rawData;
+  enum Types typeToken;
   const struct InternalError *err;
-} VoidResult;
+};
 
 typedef const char *RawCStr;
 
-typedef struct Result_ByteSlice {
-  const struct ByteSlice *rawData;
-  const struct InternalError *err;
-} Result_ByteSlice;
+typedef const int32_t *Int32Array;
 
-typedef struct Result_i32 {
-  const int32_t *rawData;
-  const struct InternalError *err;
-} Result_i32;
+void __use_typed_array(struct RawTypedArray typed_array);
 
 void initialize(const char *dir, struct NativeLogger logger);
 
-void destroy_void_result(const struct VoidResult *ptr);
+void free_buffer(const void *ptr);
 
-const struct VoidResult *put_str(RawCStr key, RawCStr value);
+const struct RawBuffer *put_str(RawCStr key, RawCStr value);
 
-const struct Result_ByteSlice *get_str(RawCStr key);
+const struct RawBuffer *get_str(RawCStr key);
 
-void destroy_str_result(const struct Result_ByteSlice *ptr);
+const struct RawBuffer *put_i32(RawCStr key, int32_t value);
 
-const struct VoidResult *put_i32(RawCStr key, int32_t value);
+const struct RawBuffer *get_i32(RawCStr key);
 
-const struct Result_i32 *get_i32(RawCStr key);
+const struct RawBuffer *put_i32_array(RawCStr key, Int32Array value, uintptr_t len);
 
-void destroy_i32_result(const struct Result_i32 *ptr);
+const struct RawBuffer *get_i32_array(RawCStr key);
--- ios/MMKV/Tests/MMKVTests/MMKVTests.swift
+++ ios/MMKV/Tests/MMKVTests/MMKVTests.swift
@@ -6,60 +6,51 @@ final class MMKVTests: XCTestCase {
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
         MMKV.initialize(dir: ".")
-        let emptyResult = MMKV.getString(key: "key_not_exists")
+        let emptyResult = MMKV.getString(key: "key_not_exists").unwrapResult()
         switch emptyResult {
         case .failure(let err):
             assertError(err: err, exceptCode: 0)
         case .success(_):
             XCTFail("Should be failure for key_not_exists")
         }
-        let ret = MMKV.putString(key: "key", value: "test_value")
-        switch ret {
-        case .failure(_):
-            XCTFail()
-        case .success(_):
-            break
-        }
-        let strResult = MMKV.getString(key: "key")
-        switch strResult {
-        case .failure(_):
-            XCTFail()
-        case .success(let value):
-            XCTAssertEqual(value, "test_value")
-        }
+        try MMKV.putString(key: "key", value: "test_value").unwrap()
+        let str = MMKV.getString(key: "key").unwrap(defalutValue: "")
+        XCTAssertEqual(str, "test_value")
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
     }
     
     func testPutAndGetInt32() throws {
         MMKV.initialize(dir: ".")
-        let emptyResult = MMKV.getInt32(key: "key_not_exists")
+        let emptyResult = MMKV.getInt32(key: "key_not_exists").unwrapResult()
         switch emptyResult {
         case .failure(let err):
             assertError(err: err, exceptCode: 0)
         case .success(_):
             XCTFail("Should be failure for key_not_exists")
         }
-        let ret = MMKV.putInt32(key: "key", value: 12)
-        switch ret {
-        case .failure(_):
-            XCTFail()
+        try MMKV.putInt32(key: "key", value: 12).unwrap()
+        let value = MMKV.getInt32(key: "key").unwrap(defalutValue: 0)
+        XCTAssertEqual(value, 12)
+    }
+    
+    func testPutAndGetInt32Array() throws {
+        MMKV.initialize(dir: ".")
+        let emptyResult = MMKV.getInt32Array(key: "key_not_exists").unwrapResult()
+        switch emptyResult {
+        case .failure(let err):
+            assertError(err: err, exceptCode: 0)
         case .success(_):
-            break
+            XCTFail("Should be failure for key_not_exists")
         }
-        let intResult = MMKV.getInt32(key: "key")
-        switch intResult {
-        case .failure(_):
-            XCTFail()
-        case .success(let value):
-            XCTAssertEqual(value, 12)
-        }
+        let array: [Int32] = [1, 2, 3, 4, 5]
+        try MMKV.putInt32Array(key: "key", value: array).unwrap()
+        let value = MMKV.getInt32Array(key: "key").unwrap(defalutValue: [])
+        XCTAssertEqual(value, array)
     }
     
     func assertError(err: MMKVError, exceptCode: Int32) {
         switch err {
-        case MMKVError.unknown:
-            XCTFail("Should not be unknown error")
         case MMKVError.native(let code, _):
             XCTAssertEqual(code, exceptCode)
         }
