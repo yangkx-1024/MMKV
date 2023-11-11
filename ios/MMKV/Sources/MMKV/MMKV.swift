ca3acf0fb0aee54eae9c804b6f5c4f997be2511c
kexuan.yang <kexuan.yang@gmail.com>
2023-11-11 16:37:47 +0800

    full API support for apple platform
    Change-Id: I6873c09b63daafaa1a67b514887f41d4142d2233

--- ios/MMKV/Sources/MMKV/MMKV.swift
+++ ios/MMKV/Sources/MMKV/MMKV.swift
@@ -18,6 +18,20 @@ private extension UnsafePointer<ByteSlice> {
     }
 }
 
+private extension UnsafePointer<RawTypedArray> {
+    func readArray<T> (source: T.Type) -> Array<T> {
+        let count = Int(self.pointee.len)
+        var array = Array<T>()
+        array.reserveCapacity(count)
+        let ptr = self.pointee.array.assumingMemoryBound(to: source)
+        let sourceArray = UnsafeBufferPointer(start: ptr, count: count)
+        for i in 0...count - 1 {
+            array.append(sourceArray[i])
+        }
+        return array
+    }
+}
+
 private extension UnsafePointer<InternalError> {
     private func code() -> Int32 {
         return Int32(self.pointee.code)
@@ -35,13 +49,17 @@ private extension UnsafePointer<InternalError> {
     }
 }
 
+private extension UnsafeRawPointer {
+    func bindType<T>(type: T.Type) -> UnsafePointer<T> {
+        self.assumingMemoryBound(to: type)
+    }
+}
+
 public class ResultWrapper<T> {
     private let rawBuffer: UnsafePointer<RawBuffer>
-    private let decode: (RawBuffer) -> T
     
-    init(rawBuffer: UnsafePointer<RawBuffer>, decode: @escaping (RawBuffer) -> T) {
+    init(rawBuffer: UnsafePointer<RawBuffer>) {
         self.rawBuffer = rawBuffer
-        self.decode = decode
     }
     
     deinit {
@@ -52,7 +70,37 @@ public class ResultWrapper<T> {
         if rawBuffer.pointee.err != nil {
             return Result.failure(rawBuffer.pointee.err.asMMKVError())
         }
-        return Result.success(decode(rawBuffer.pointee))
+        if rawBuffer.pointee.rawData == nil {
+            return Result.success(() as! T)
+        }
+        let rawData = rawBuffer.pointee.rawData!
+        let data: Any = switch rawBuffer.pointee.typeToken {
+        case RustMMKV.I32:
+            Int32(rawData.bindType(type: Int32.self).pointee)
+        case RustMMKV.Str:
+            rawData.bindType(type: ByteSlice.self).asString()!
+        case RustMMKV.Bool:
+            Bool(rawData.bindType(type: Bool.self).pointee)
+        case RustMMKV.I64:
+            Int64(rawData.bindType(type: Int64.self).pointee)
+        case RustMMKV.F32:
+            Float32(rawData.bindType(type: Float32.self).pointee)
+        case RustMMKV.F64:
+            Float64(rawData.bindType(type: Float64.self).pointee)
+        case RustMMKV.ByteArray:
+            rawData.bindType(type: RawTypedArray.self).readArray(source: UInt8.self)
+        case RustMMKV.I32Array:
+            rawData.bindType(type: RawTypedArray.self).readArray(source: Int32.self)
+        case RustMMKV.I64Array:
+            rawData.bindType(type: RawTypedArray.self).readArray(source: Int64.self)
+        case RustMMKV.F32Array:
+            rawData.bindType(type: RawTypedArray.self).readArray(source: Float32.self)
+        case RustMMKV.F64Array:
+            rawData.bindType(type: RawTypedArray.self).readArray(source: Float64.self)
+        default:
+            fatalError("should not happen")
+        }
+        return Result.success(data as! T)
     }
     
     public func unwrap(defalutValue: T) -> T {
@@ -75,8 +123,8 @@ public class ResultWrapper<T> {
 }
 
 private extension UnsafePointer<RawBuffer> {
-    func intoResultWrapper<T>(decoder: @escaping (RawBuffer) -> T) -> ResultWrapper<T> {
-        return ResultWrapper(rawBuffer: self, decode: decoder)
+    func intoResultWrapper<T>() -> ResultWrapper<T> {
+        return ResultWrapper(rawBuffer: self)
     }
 }
 
@@ -121,6 +169,7 @@ private class LogWrapper {
 
 public class MMKV {
     
+    private init() {}
     private static let logger = Logger(label: "net.yangkx.MMKV")
     private static let logwrapper = LogWrapper(logger: logger)
     
@@ -128,51 +177,99 @@ public class MMKV {
         RustMMKV.initialize(dir, logwrapper.toNativeLogger())
     }
     
+    public static func close() {
+        RustMMKV.close_instance()
+    }
+    
+    public static func clearData() {
+        RustMMKV.clear_data()
+    }
+    
     public static func putString(key: String, value: String) -> ResultWrapper<Void> {
-        RustMMKV.put_str(key, value).intoResultWrapper() {
-            (_) -> Void in
-        }
+        RustMMKV.put_str(key, value).intoResultWrapper()
     }
     
     public static func getString(key: String) -> ResultWrapper<String> {
-        return RustMMKV.get_str(key).intoResultWrapper() {
-            (buffer) -> String in
-            return buffer.rawData.assumingMemoryBound(to: ByteSlice.self).asString()!
-        }
+        return RustMMKV.get_str(key).intoResultWrapper()
+    }
+    
+    public static func putBool(key: String, value: Bool) -> ResultWrapper<Void> {
+        return RustMMKV.put_bool(key, value).intoResultWrapper()
+    }
+    
+    public static func getBool(key: String) -> ResultWrapper<Bool> {
+        return RustMMKV.get_bool(key).intoResultWrapper()
     }
     
     public static func putInt32(key: String, value: Int32) -> ResultWrapper<Void> {
-        return RustMMKV.put_i32(key, value).intoResultWrapper() {
-            (_) -> Void in
-        }
+        return RustMMKV.put_i32(key, value).intoResultWrapper()
     }
     
     public static func getInt32(key: String) -> ResultWrapper<Int32> {
-        return RustMMKV.get_i32(key).intoResultWrapper() {
-            (buffer) -> Int32 in
-            let value = buffer.rawData.assumingMemoryBound(to: Int32.self)
-            return Int32(value.pointee)
-        }
+        return RustMMKV.get_i32(key).intoResultWrapper()
+    }
+    
+    public static func putInt64(key: String, value: Int64) -> ResultWrapper<Void> {
+        return RustMMKV.put_i64(key, value).intoResultWrapper()
+    }
+    
+    public static func getInt64(key: String) -> ResultWrapper<Int64> {
+        return RustMMKV.get_i64(key).intoResultWrapper()
+    }
+
+    public static func putFloat32(key: String, value: Float32) -> ResultWrapper<Void> {
+        return RustMMKV.put_f32(key, value).intoResultWrapper()
+    }
+    
+    public static func getFloat32(key: String) -> ResultWrapper<Float32> {
+        return RustMMKV.get_f32(key).intoResultWrapper()
+    }
+    
+    public static func putFloat64(key: String, value: Float64) -> ResultWrapper<Void> {
+        return RustMMKV.put_f64(key, value).intoResultWrapper()
+    }
+    
+    public static func getFloat64(key: String) -> ResultWrapper<Float64> {
+        return RustMMKV.get_f64(key).intoResultWrapper()
+    }
+    
+    public static func putByteArray(key: String, value: Array<UInt8>) -> ResultWrapper<Void> {
+        return RustMMKV.put_byte_array(key, value, UInt(value.count)).intoResultWrapper()
+    }
+    
+    public static func getByteArray(key: String) -> ResultWrapper<[UInt8]> {
+        return RustMMKV.get_byte_array(key).intoResultWrapper()
     }
     
     public static func putInt32Array(key: String, value: Array<Int32>) -> ResultWrapper<Void> {
-        return RustMMKV.put_i32_array(key, value, UInt(value.count)).intoResultWrapper() {
-            (_) -> Void in
-        }
+        return RustMMKV.put_i32_array(key, value, UInt(value.count)).intoResultWrapper()
     }
     
     public static func getInt32Array(key: String) -> ResultWrapper<[Int32]> {
-        return RustMMKV.get_i32_array(key).intoResultWrapper(decoder: {
-            (buffer) -> Array<Int32> in
-            let ptr = buffer.rawData.assumingMemoryBound(to: RawTypedArray.self)
-            let count = Int(ptr.pointee.len)
-            var output = Array<Int32>(repeating: 0, count: count)
-            let array = ptr.pointee.array.assumingMemoryBound(to: Int32.self)
-            let buffer = UnsafeBufferPointer<Int32>(start: array, count: count)
-            for i in 0...count - 1 {
-                output[i] = buffer[i]
-            }
-            return output
-        })
+        return RustMMKV.get_i32_array(key).intoResultWrapper()
+    }
+    
+    public static func putInt64Array(key: String, value: Array<Int64>) -> ResultWrapper<Void> {
+        return RustMMKV.put_i64_array(key, value, UInt(value.count)).intoResultWrapper()
+    }
+    
+    public static func getInt64Array(key: String) -> ResultWrapper<[Int64]> {
+        return RustMMKV.get_i64_array(key).intoResultWrapper()
+    }
+    
+    public static func putFloat32Array(key: String, value: Array<Float32>) -> ResultWrapper<Void> {
+        return RustMMKV.put_f32_array(key, value, UInt(value.count)).intoResultWrapper()
+    }
+    
+    public static func getFloat32Array(key: String) -> ResultWrapper<[Float]> {
+        return RustMMKV.get_f32_array(key).intoResultWrapper()
+    }
+    
+    public static func putFloat64Array(key: String, value: Array<Float64>) -> ResultWrapper<Void> {
+        return RustMMKV.put_f64_array(key, value, UInt(value.count)).intoResultWrapper()
+    }
+    
+    public static func getFloat64Array(key: String) -> ResultWrapper<[Float64]> {
+        return RustMMKV.get_f64_array(key).intoResultWrapper()
     }
 }
