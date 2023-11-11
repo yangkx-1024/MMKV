1a109a2c7862a9ee17515924f140c4b5cbf78be9
kexuan.yang <kexuan.yang@gmail.com>
2023-11-11 16:37:47 +0800

    full API support for apple platform
    Change-Id: I6873c09b63daafaa1a67b514887f41d4142d2233

--- src/clib/mod.rs
+++ src/clib/mod.rs
@@ -9,15 +9,21 @@ pub mod clib {
 
     use crate::{Error, Logger, LogLevel, MMKV};
 
-    const LOG_TAG: &str = "MMKV:CLIB";
+    const LOG_TAG: &str = "MMKV:FFI";
+
+    pub type CByteArray = *const u8;
+    pub type CI32Array = *const i32;
+    pub type CI64Array = *const i64;
+    pub type CF32Array = *const f32;
+    pub type CF64Array = *const f64;
 
     #[repr(C)]
     #[derive(Debug)]
     #[allow(dead_code)]
     pub enum Types {
         I32,
-        STR,
-        BYTE,
+        Str,
+        Bool,
         I64,
         F32,
         F64,
@@ -77,20 +83,27 @@ pub mod clib {
     }
 
     impl RawTypedArray {
-        fn new_with_i32_array(array: Vec<i32>) -> Self {
+        fn new<T: Sized>(array: Vec<T>, type_token: Types) -> Self {
             let boxed = array.into_boxed_slice();
             let ptr = boxed.as_ptr();
             let len = boxed.len();
             std::mem::forget(boxed);
             RawTypedArray {
                 array: ptr as *mut _,
-                type_token: Types::I32Array,
+                type_token,
                 len,
             }
         }
 
         unsafe fn drop_array(&mut self) {
             let _: Box<dyn Any> = match self.type_token {
+                Types::ByteArray => {
+                    Box::from_raw(
+                        std::slice::from_raw_parts_mut(
+                            self.array as *mut u8, self.len,
+                        ).as_mut_ptr()
+                    )
+                }
                 Types::I32Array => {
                     Box::from_raw(
                         std::slice::from_raw_parts_mut(
@@ -98,7 +111,28 @@ pub mod clib {
                         ).as_mut_ptr()
                     )
                 }
-                _ => { Box::new(()) }
+                Types::I64Array => {
+                    Box::from_raw(
+                        std::slice::from_raw_parts_mut(
+                            self.array as *mut i64, self.len,
+                        ).as_mut_ptr()
+                    )
+                }
+                Types::F32Array => {
+                    Box::from_raw(
+                        std::slice::from_raw_parts_mut(
+                            self.array as *mut f32, self.len,
+                        ).as_mut_ptr()
+                    )
+                }
+                Types::F64Array => {
+                    Box::from_raw(
+                        std::slice::from_raw_parts_mut(
+                            self.array as *mut f64, self.len,
+                        ).as_mut_ptr()
+                    )
+                }
+                _ => { panic!("can't match type of array") }
             };
         }
     }
@@ -187,16 +221,28 @@ pub mod clib {
                 return;
             }
             let _: Box<dyn Any> = match self.typeToken {
-                Types::STR => {
-                    Box::from_raw(self.rawData as *mut ByteSlice)
-                }
                 Types::I32 => {
                     Box::from_raw(self.rawData as *mut i32)
                 }
-                Types::I32Array => {
+                Types::Str => {
+                    Box::from_raw(self.rawData as *mut ByteSlice)
+                }
+                Types::Bool => {
+                    Box::from_raw(self.rawData as *mut bool)
+                }
+                Types::I64 => {
+                    Box::from_raw(self.rawData as *mut i64)
+                }
+                Types::F32 => {
+                    Box::from_raw(self.rawData as *mut f32)
+                }
+                Types::F64 => {
+                    Box::from_raw(self.rawData as *mut f64)
+                }
+                Types::ByteArray | Types::I32Array | Types::I64Array | Types::F32Array
+                    | Types::F64Array => {
                     Box::from_raw(self.rawData as *mut RawTypedArray)
                 }
-                _ => { Box::new(()) }
             };
         }
 
@@ -286,27 +332,72 @@ pub mod clib {
             let value_str = unsafe { CStr::from_ptr($value) }.to_str().unwrap();
             MMKV::put_str($key, value_str)
         }};
+        ($key:expr, $value:expr, bool) => {
+            MMKV::put_bool($key, $value)
+        };
         ($key:expr, $value:expr, i32) => {
             MMKV::put_i32($key, $value)
         };
-        ($key:expr, $value:expr, $len:expr, Int32Array) => {{
-            let array = unsafe {
-                std::slice::from_raw_parts($value, $len)
-            };
-            MMKV::put_i32_array($key, array)
-        }};
+        ($key:expr, $value:expr, i64) => {
+            MMKV::put_i64($key, $value)
+        };
+        ($key:expr, $value:expr, f32) => {
+            MMKV::put_f32($key, $value)
+        };
+        ($key:expr, $value:expr, f64) => {
+            MMKV::put_f64($key, $value)
+        };
+        ($key:expr, $value:expr, $len:expr, CByteArray) => {
+            MMKV::put_byte_array($key, unsafe { std::slice::from_raw_parts($value, $len) })
+        };
+        ($key:expr, $value:expr, $len:expr, CI32Array) => {
+            MMKV::put_i32_array($key, unsafe { std::slice::from_raw_parts($value, $len) })
+        };
+        ($key:expr, $value:expr, $len:expr, CI64Array) => {
+            MMKV::put_i64_array($key, unsafe { std::slice::from_raw_parts($value, $len) })
+        };
+        ($key:expr, $value:expr, $len:expr, CF32Array) => {
+            MMKV::put_f32_array($key, unsafe { std::slice::from_raw_parts($value, $len) })
+        };
+        ($key:expr, $value:expr, $len:expr, CF64Array) => {
+            MMKV::put_f64_array($key, unsafe { std::slice::from_raw_parts($value, $len) })
+        };
     }
 
     macro_rules! mmkv_get {
         ($key:expr, ByteSlice) => {
             MMKV::get_str($key).map(|value| ByteSlice::new(value))
         };
+        ($key:expr, bool) => {
+            MMKV::get_bool($key)
+        };
         ($key:expr, i32) => {
             MMKV::get_i32($key)
         };
-        ($key:expr, Int32Array) => {
-            MMKV::get_i32_array($key).map(|value| RawTypedArray::new_with_i32_array(value))
-        }
+        ($key:expr, i64) => {
+            MMKV::get_i64($key)
+        };
+        ($key:expr, f32) => {
+            MMKV::get_f32($key)
+        };
+        ($key:expr, f64) => {
+            MMKV::get_f64($key)
+        };
+        ($key:expr, CByteArray) => {
+            MMKV::get_byte_array($key).map(|value| RawTypedArray::new(value, Types::ByteArray))
+        };
+        ($key:expr, CI32Array) => {
+            MMKV::get_i32_array($key).map(|value| RawTypedArray::new(value, Types::I32Array))
+        };
+        ($key:expr, CI64Array) => {
+            MMKV::get_i64_array($key).map(|value| RawTypedArray::new(value, Types::I64Array))
+        };
+        ($key:expr, CF32Array) => {
+            MMKV::get_f32_array($key).map(|value| RawTypedArray::new(value, Types::F32Array))
+        };
+        ($key:expr, CF64Array) => {
+            MMKV::get_f64_array($key).map(|value| RawTypedArray::new(value, Types::F64Array))
+        };
     }
 
     fn map_error(key: &str, e: Error, log: &str) -> Box<InternalError> {
@@ -384,16 +475,57 @@ pub mod clib {
         let _ = RawBuffer::from_raw(ptr as *mut RawBuffer);
     }
 
-    impl_put!(put_str, RawCStr, Types::STR, "put string");
+    #[no_mangle]
+    pub extern fn close_instance() {
+        MMKV::close()
+    }
 
-    impl_get!(get_str, ByteSlice, Types::STR, "get string");
+    #[no_mangle]
+    pub extern fn clear_data() {
+        MMKV::clear_data()
+    }
+
+    impl_put!(put_str, RawCStr, Types::Str, "put string");
+
+    impl_get!(get_str, ByteSlice, Types::Str, "get string");
+
+    impl_put!(put_bool, bool, Types::Bool, "put bool");
+
+    impl_get!(get_bool, bool, Types::Bool, "get bool");
 
     impl_put!(put_i32, i32, Types::I32, "put i32");
 
     impl_get!(get_i32, i32, Types::I32, "get i32");
 
-    type Int32Array = *const i32;
-    impl_put_typed_array!(put_i32_array, Int32Array, Types::I32Array, "put i32 array");
+    impl_put!(put_i64, i64, Types::I64, "put i64");
 
-    impl_get!(get_i32_array, Int32Array, Types::I32Array, "get i32 array");
+    impl_get!(get_i64, i64, Types::I64, "get i64");
+
+    impl_put!(put_f32, f32, Types::F32, "put f32");
+
+    impl_get!(get_f32, f32, Types::F32, "get f32");
+
+    impl_put!(put_f64, f64, Types::F64, "put f64");
+
+    impl_get!(get_f64, f64, Types::F64, "get f64");
+
+    impl_put_typed_array!(put_byte_array, CByteArray, Types::ByteArray, "put byte array");
+
+    impl_get!(get_byte_array, CByteArray, Types::ByteArray, "get byte array");
+
+    impl_put_typed_array!(put_i32_array, CI32Array, Types::I32Array, "put i32 array");
+
+    impl_get!(get_i32_array, CI32Array, Types::I32Array, "get i32 array");
+
+    impl_put_typed_array!(put_i64_array, CI64Array, Types::I64Array, "put i64 array");
+
+    impl_get!(get_i64_array, CI64Array, Types::I64Array, "get i64 array");
+
+    impl_put_typed_array!(put_f32_array, CF32Array, Types::F32Array, "put f32 array");
+
+    impl_get!(get_f32_array, CF32Array, Types::F32Array, "get f32 array");
+
+    impl_put_typed_array!(put_f64_array, CF64Array, Types::F64Array, "put f64 array");
+
+    impl_get!(get_f64_array, CF64Array, Types::F64Array, "get f64 array");
 }
