18522733e93744f09163d30f2f53d6b86390571d
kexuan.yang <kexuan.yang@gmail.com>
2023-12-01 16:42:48 +0800

    Update android build script
    Change-Id: I5465c48cbdcd8b3cd41c916036bd559620c00801

--- build_android.sh
+++ build_android.sh
@@ -16,40 +16,25 @@ export CARGO_TARGET_ARMV7_LINUX_ANDROIDEABI_LINKER=$ANDROID_NDK_TOOLCHAINS_PATH/
 # target.x86_64-linux-android
 export CARGO_TARGET_X86_64_LINUX_ANDROID_LINKER=$ANDROID_NDK_TOOLCHAINS_PATH/x86_64-linux-android33-clang
 
-echo "Building with default feature......"
-echo "Building aarch64-linux-android......"
-cargo build --target aarch64-linux-android
-cp target/aarch64-linux-android/debug/libmmkv.so android/library/src/debug/jniLibs/arm64-v8a/libmmkv.so
-cargo build --target aarch64-linux-android --release
-cp target/aarch64-linux-android/release/libmmkv.so android/library/src/main/jniLibs/arm64-v8a/libmmkv.so
+targets_dic=(
+  "aarch64-linux-android:arm64-v8a"
+  "armv7-linux-androideabi:armeabi-v7a"
+  "x86_64-linux-android:x86_64"
+)
 
-echo "Building armv7-linux-androideabi......"
-cargo build --target armv7-linux-androideabi
-cp target/armv7-linux-androideabi/debug/libmmkv.so android/library/src/debug/jniLibs/armeabi-v7a/libmmkv.so
-cargo build --target armv7-linux-androideabi --release
-cp target/armv7-linux-androideabi/release/libmmkv.so android/library/src/main/jniLibs/armeabi-v7a/libmmkv.so
-
-echo "Building x86_64-linux-android......"
-cargo build --target x86_64-linux-android
-cp target/x86_64-linux-android/debug/libmmkv.so android/library/src/debug/jniLibs/x86_64/libmmkv.so
-cargo build --target x86_64-linux-android --release
-cp target/x86_64-linux-android/release/libmmkv.so android/library/src/main/jniLibs/x86_64/libmmkv.so
-
-echo "Building with feature encryption...."
-echo "Building aarch64-linux-android......"
-cargo build --features encryption --target aarch64-linux-android
-cp target/aarch64-linux-android/debug/libmmkv.so android/library-encrypt/src/debug/jniLibs/arm64-v8a/libmmkv.so
-cargo build --features encryption --target aarch64-linux-android --release
-cp target/aarch64-linux-android/release/libmmkv.so android/library-encrypt/src/main/jniLibs/arm64-v8a/libmmkv.so
-
-echo "Building armv7-linux-androideabi......"
-cargo build --features encryption --target armv7-linux-androideabi
-cp target/armv7-linux-androideabi/debug/libmmkv.so android/library-encrypt/src/debug/jniLibs/armeabi-v7a/libmmkv.so
-cargo build --features encryption --target armv7-linux-androideabi --release
-cp target/armv7-linux-androideabi/release/libmmkv.so android/library-encrypt/src/main/jniLibs/armeabi-v7a/libmmkv.so
-
-echo "Building x86_64-linux-android......"
-cargo build --features encryption --target x86_64-linux-android
-cp target/x86_64-linux-android/debug/libmmkv.so android/library-encrypt/src/debug/jniLibs/x86_64/libmmkv.so
-cargo build --features encryption --target x86_64-linux-android --release
-cp target/x86_64-linux-android/release/libmmkv.so android/library-encrypt/src/main/jniLibs/x86_64/libmmkv.so
+for ITEM in "${targets_dic[@]}" ; do
+  TARGET=${ITEM%%:*}
+  ANDROID_TARGET=${ITEM##*:}
+  echo "Building $TARGET......"
+  rustup target add "$TARGET"
+  echo "Building with default feature......"
+  cargo build --target "$TARGET"
+  cargo build --target "$TARGET" --release
+  cp target/"$TARGET"/debug/libmmkv.so android/library/src/debug/jniLibs/"$ANDROID_TARGET"/libmmkv.so
+  cp target/"$TARGET"/release/libmmkv.so android/library/src/main/jniLibs/"$ANDROID_TARGET"/libmmkv.so
+  echo "Building with feature encryption...."
+  cargo build --features encryption --target "$TARGET"
+  cargo build --features encryption --target "$TARGET" --release
+  cp target/"$TARGET"/debug/libmmkv.so android/library-encrypt/src/debug/jniLibs/"$ANDROID_TARGET"/libmmkv.so
+  cp target/"$TARGET"/release/libmmkv.so android/library-encrypt/src/main/jniLibs/"$ANDROID_TARGET"/libmmkv.so
+done
