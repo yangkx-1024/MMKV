8815cfebd9c4fadb73e90adce169aa9ad00be59e
Kexuan Yang <kexuan.yang@gmail.com>
2024-01-13 13:52:54 +0800

    Upgrade Android compose version
    Signed-off-by: Kexuan Yang <kexuan.yang@gmail.com>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -62,7 +62,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.8"
     }
     flavorDimensions += "feature"
     productFlavors {
